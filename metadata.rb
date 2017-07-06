c7442969967e585ac21f2a9e7418653f270656d1
Jason Reid <jason.reid@r3labs.io>
2017-07-06 13:36:15 +0100

    install and test apache2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures chef-inspec'
 long_description 'Installs/Configures chef-inspec'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,6 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/chef-inspec'
+
+depends "apt"
+depends "apache2"
