ec992fd0a93190cfffd0d073fd3af443d0097c61
Jason Reid <jason.reid@r3labs.io>
2017-07-07 11:28:18 +0100

    cleaned up tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,3 @@
-# # encoding: utf-8
-
-# Inspec test for recipe chef-inspec::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-#unless os.windows?
-  # This is an example test, replace with your own test.
-#  describe user('root')
-#    it { should exist }
-#  end
-#end
-
-# This is an example test, replace it with your own test.
 describe port(80) do
   it { should be_listening }
 end
