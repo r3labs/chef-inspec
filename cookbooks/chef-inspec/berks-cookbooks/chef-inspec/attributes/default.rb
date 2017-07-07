e50a28c8154910c44db709d83b6878b1b308eaef
Jason Reid <jason.reid@r3labs.io>
2017-07-07 12:18:54 +0100

    stupid me

---
+++ cookbooks/chef-inspec/berks-cookbooks/chef-inspec/attributes/default.rb
@@ -0,0 +1,2 @@
+default['java']['install_flavor'] = 'openjdk'  # this is the default, but let's be EXPLICIT!
+default['java']['jdk_version'] = '7'
---
+++ cookbooks/chef-inspec/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: chef-inspec
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'apt'
+include_recipe 'apache2'
